bca9d1dcf23bca478418cafc7de146de42a44eca
Ayelet Dahan <anativ@gmail.com>
2014-12-11 22:40:21 +0200

    First angular service, Starting from the testing, more or less

---
+++ webui/static/webui/api_services/episodes.srv.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular.module('vidtrage.api.episodes', [])
+        .service('EpisodesService', [
+            '$log', '$http',
+            function($log, $http) {
+                var episode = {}; // Public API object
+
+
+
+
+
+                return episodes;
+            }]);
+})();
