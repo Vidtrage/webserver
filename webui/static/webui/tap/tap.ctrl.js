adaae2e4e744020ad10a96870b3cdf8dbe209bed
Ayelet Dahan <anativ@gmail.com>
2014-12-25 08:43:49 +0200

    Minor improvements to HTML. Added videogular library to help solve angular's HTML5 video tag problem (does not handle the src/ng-src property well)

--- webui/static/webui/tap/tap.ctrl.js
+++ webui/static/webui/tap/tap.ctrl.js
@@ -1,13 +1,28 @@
 (function() {
     'use strict';
 
-    angular.module('vidtrage.tap.ctrl', [])
+    angular.module('vidtrage.tap', [])
         .controller('TAPController', ['$log', '$scope', function($log, $scope) {
 
-            $scope.episodes = [{'name': 'myep'}];
-            $scope.ads = [{'name': 'myad'}];
+            $scope.episodes = [{'name': 'myep',
+                'source': {
+                    'path': 'http://techslides.com/demos/sample-videos/small.webm',
+                    'type': 'video/webm'
+                }
+            }];
+            $scope.ads = [{'name': 'myad',
+                'source': {
+                    'path': 'http://techslides.com/demos/sample-videos/small.webm',
+                    'type': 'video/webm'
+                }
+            }];
             $scope.resources = [];
+            $scope.videoToPlay = {'path': '', 'type': ''};
+
+            $scope.play = function(videoEntity) {
+                $scope.videoToPlay = videoEntity.source;
+            };
 
 
         }]);
-});
+})();
