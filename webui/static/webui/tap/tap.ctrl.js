5e4e569a3b50ca9e75b212dfd11584e00e0f56a4
Ayelet Dahan <anativ@gmail.com>
2014-12-26 23:40:35 +0200

    switching videos between ad and episode works.

--- webui/static/webui/tap/tap.ctrl.js
+++ webui/static/webui/tap/tap.ctrl.js
@@ -5,22 +5,16 @@
         .controller('TAPController', ['$log', '$scope', function($log, $scope) {
 
             $scope.episodes = [{'name': 'myep',
-                'sources': [{
-                    'path': 'http://techslides.com/demos/sample-videos/small.webm',
-                    'type': 'video/webm'
-                }]
+                'source': 'http://techslides.com/demos/sample-videos/small.webm'
             }];
             $scope.ads = [{'name': 'myad',
-                'sources': [{
-                    'path': 'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4',
-                    'type': 'video/mp4'
-                }]
+                'source': 'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4'
             }];
             $scope.resources = [];
             $scope.videoToPlay = [{'path': '', 'type': ''}];
 
             $scope.play = function(videoEntity) {
-                $scope.videoToPlay = videoEntity.sources;
+                $scope.videoToPlay = videoEntity.source;
             };
 
 
--- webui/static/webui/tap/video.drctv.js
+++ webui/static/webui/tap/video.drctv.js
@@ -8,39 +8,17 @@
                 template: '<video controls></video>',
                 replace: true,
                 scope: {
-                    sources: '='
+                    source: '='
                 },
                 link: function($scope, videoElement, attributes) {
 
 
-                    $scope.$watch('sources', function(newSources) {
-                        if (!Array.isArray(newSources)) {
-                            $log.error("tapVideo directive sources is not an array", newSources);
-                            return;
-                        }
-
-                        clearControlSources();
-                        newSources.forEach(function(source) {
-                            addSourceToVideo(source);
-                        });
+                    $scope.$watch('source', function(newSource) {
+                        changeVideoSource(newSource);
                     });
 
-                    var clearControlSources = function() {
-                        videoElement.children().remove();
-                    };
-
-                    var addSourceToVideo = function(source) {
-                        if (!angular.isObject(source) || !angular.isString(source.path) || !angular.isString(source.type)) {
-                            $log.error('tapVideo directive sources contains invalid source', source);
-                            return null;
-                        }
-
-                        var sourceElement = angular.element('<source />');
-                        sourceElement.attr('src', source.path);
-                        sourceElement.attr('type', source.type);
-                        videoElement.append(sourceElement);
-
-                        return sourceElement;
+                    var changeVideoSource = function(source) {
+                        videoElement.attr('src', source);
                     };
                 }
             }
